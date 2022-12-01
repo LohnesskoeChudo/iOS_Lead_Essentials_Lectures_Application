68c30a25fe67ee338ebcc37c0cc35f6c3881beb3
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2022-12-01 17:18:50 +0300

    load requests with proper url

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,22 +9,43 @@ import XCTest
 
 final class RemoteFeedLoader {
     private let client: HTTPClient
+    private let url: URL
     
-    init(client: HTTPClient) {
+    func load() {
+        client.get(from: url)
+    }
+    
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
 }
 
 class HTTPClient {
     var requestedUrl: URL?
+    
+    func get(from url: URL) {
+        requestedUrl = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
-    func test_sut_doesNotRequestOnCreation() throws {
+    func test_sut_doesNotRequestOnCreation() {
+        let url = URL(string: "http://any-url.com")!
         let client = HTTPClient()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedUrl)
+    }
+    
+    func test_load_requestsWithProperUrl() {
+        let url = URL(string: "http://given-url.com")!
+        let client = HTTPClient()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedUrl, url)
     }
 }
