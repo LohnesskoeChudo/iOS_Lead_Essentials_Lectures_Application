07f2414b1f90a5facd9b13a5e7b4cf774f3d416f
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2022-12-06 00:27:38 +0300

    move 'URLSessionHTTPClient' to own file

---
+++ EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Василий Клецкин on 06.12.2022.
+//
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    struct UnexpectedResponse: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPResponse) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedResponse()))
+            }
+        }.resume()
+    }
+}
--- EssentialFeed/EssentialFeedTests/Feed API/HTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/HTTPClientTests.swift
@@ -8,31 +8,7 @@
 import XCTest
 import EssentialFeed
 
-final class URLSessionHTTPClient: HTTPClient {
-    
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedResponse: Error {}
-    
-    func get(from url: URL, completion: @escaping (HTTPResponse) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedResponse()))
-            }
-        }.resume()
-    }
-}
-
 final class HTTPClientTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
         URLProtocolStub.startInterception()
