95fccdbd477d0621ff7ca40851377413945a4b53
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2022-12-20 19:09:48 +0300

    move shared helpers to shared scope

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -8,21 +8,6 @@
 import EssentialFeed
 import Foundation
 
-func anyFeed() -> (models: [FeedImage], locals: [LocalFeedImage]) {
-    let models = [uniqueImage(), uniqueImage(), uniqueImage()]
-    let locals = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-    return (models, locals)
-}
-
-func uniqueImage() -> FeedImage {
-    FeedImage(
-        id: UUID(),
-        description: "any description",
-        location: "any location",
-        url: anyUrl()
-    )
-}
-
 extension Date {
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
--- EssentialFeed/EssentialFeedTests/Helpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers.swift
@@ -8,6 +8,21 @@
 import XCTest
 import EssentialFeed
 
+func anyFeed() -> (models: [FeedImage], locals: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage(), uniqueImage()]
+    let locals = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    return (models, locals)
+}
+
+func uniqueImage() -> FeedImage {
+    FeedImage(
+        id: UUID(),
+        description: "any description",
+        location: "any location",
+        url: anyUrl()
+    )
+}
+
 func anyUrl() -> URL {
     URL(string: "http://any-url.com")!
 }
