51a7bf80e195ca3dc76bffd76fd921c6d2b89f2d
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2022-12-20 18:46:21 +0300

    target for feed cache integration tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Василий Клецкин on 20.12.2022.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    func testExample() throws {
+ 
+    }
+}
