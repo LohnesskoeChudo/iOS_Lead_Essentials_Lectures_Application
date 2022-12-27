5d57cf54aed0b1344c88805efd637fb061973c2c
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2022-12-27 07:10:30 +0300

    connect outlets to feed cell

---
+++ Prototype/Prototype/FeedCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedCell.swift
+//  Prototype
+//
+//  Created by Василий Клецкин on 27.12.2022.
+//
+
+import UIKit
+
+final class FeedCell: UITableViewCell {
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var feedImageView: UIImageView!
+}
